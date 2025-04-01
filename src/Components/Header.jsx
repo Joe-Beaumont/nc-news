23af308c823b0cdf802473ffcdd9fed3681d03b9
Joe Beaumont <joseph.beaumont@live.co.uk>
2025-04-01 10:14:16 +0100

    Reapplied changes made in Routers branch as they had been reversed

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import Nav from "../Nav"
+
+function Header() {
+    return (
+        <>
+            <h1 id='Heading'>NC-News</h1>
+            <Nav/>
+        </>
+    )
+}
+
+export default Header
