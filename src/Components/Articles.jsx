e36a5f9b2523ae1d5b034e2f3b739ccb0d604e99
Joe Beaumont <joseph.beaumont@live.co.uk>
2025-04-04 17:00:03 +0100

    Started implementation of Dropdown buttons for sorting of articles, API call returning error although URL seems right

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,44 +1,90 @@
 import { useState, useEffect } from "react";
-import { getArticles, getArticlesQueries } from "../Api";
+import { ArticleQueries, getArticles } from "../Api";
 import React from 'react'
 import { ArticleCard, SingleArticleCard } from "../Cards/ArticleCard";
 import { useParams, useSearchParams } from "react-router"
 import { ErrorComponent } from "./Error";
+import { ButtonGroup, Dropdown } from "react-bootstrap";
+
 
 export function GetArticles() {
     const [articles, setArticles] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
     const [error, setError] = useState(null)
-    const [searchParams] = useSearchParams()
+    const [sortBy, setSortBy] = useState("date")
+    const [order, setOrder] = useState("desc")
+    const [searchParams, setSearchParams] = useSearchParams()
 
-    const filterQuery = searchParams.get("filter")
-    const byQuery = searchParams.get("by")
+    function handleSortChange(selectedSort) {
+        setSortBy(selectedSort)
+        searchParams.set("sort", selectedSort)
+        setSearchParams(searchParams)
+    }
+    function handleOrderChange(selectedOrder) {
+        setOrder(selectedOrder)
+        searchParams.set("order", selectedOrder)
+        setSearchParams(searchParams)
+    }
+
+    const params = {
+        filter: searchParams.get("filter"),
+        by: searchParams.get("by"),
+        sort: searchParams.get("sort"),
+        order: searchParams.get("order")
+    };
+
 
     useEffect(() => {
-        if (filterQuery) {
-            getArticlesQueries(filterQuery, byQuery)
-            .then((articles) => {
-                setArticles(articles)
-            })
-            .catch((error) => {
-                setError(error)
-            })
-            .finally(() => {
-                setIsLoading(false)
-            })
+        if (params.filter) {
+            if (params.sort) {
+                ArticleQueries(params)
+                    .then((articles) => {
+                        setArticles(articles)
+                    })
+                    .catch((error) => {
+                        setError(error)
+                    })
+                    .finally(() => {
+                        setIsLoading(false)
+                    })
+            } else {
+                ArticleQueries(params)
+                    .then((articles) => {
+                        setArticles(articles)
+                    })
+                    .catch((error) => {
+                        setError(error)
+                    })
+                    .finally(() => {
+                        setIsLoading(false)
+                    })
+            }
         } else {
-        getArticles()
-            .then((allArticles) => {
-                setArticles(allArticles);
-            })
-            .catch((error) => {
-                setError(error)
-            })
-            .finally(() => {
-                setIsLoading(false)
-            })
+            if (params.sort) {
+                ArticleQueries(params)
+                    .then((articles) => {
+                        setArticles(articles)
+                    })
+                    .catch((error) => {
+                        setError(error)
+                    })
+                    .finally(() => {
+                        setIsLoading(false)
+                    })
+            } else {
+                getArticles()
+                    .then((allArticles) => {
+                        setArticles(allArticles);
+                    })
+                    .catch((error) => {
+                        setError(error)
+                    })
+                    .finally(() => {
+                        setIsLoading(false)
+                    })
+            }
         }
-    }, [filterQuery, byQuery])
+    }, [articles])
 
     if (isLoading) {
         return <p>Loading...</p>
@@ -49,6 +95,26 @@ export function GetArticles() {
     }
 
     return (
+        <div>
+ <Dropdown as={ButtonGroup}>
+    <Dropdown.Toggle variant = "secondary" id="Sort">
+        Sort_by
+    </Dropdown.Toggle>
+    <Dropdown.Menu>
+        <Dropdown.Item onClick={() => handleSortChange("created_at")}>Date</Dropdown.Item>
+        <Dropdown.Item onClick={() => handleSortChange("comment_count")}>Comment Count</Dropdown.Item>
+        <Dropdown.Item onClick={() => handleSortChange("votes")}>Votes</Dropdown.Item>
+    </Dropdown.Menu>
+ </Dropdown>
+ <Dropdown>
+    <Dropdown.Toggle variant = "secondary" id="Order">
+        Desc
+    </Dropdown.Toggle>
+    <Dropdown.Menu>
+        <Dropdown.Item onClick={() => handleOrderChange("asc")}>Asc</Dropdown.Item>
+        <Dropdown.Item onClick={() => handleOrderChange("desc")}>Desc</Dropdown.Item>
+    </Dropdown.Menu>
+ </Dropdown>
         <ul className="List">
             {articles.map((article) => {
                 return (
@@ -61,6 +127,7 @@ export function GetArticles() {
                 )
             })}
         </ul>
+        </div>
     )
 }
 
