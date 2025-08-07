2aec86def6bc0a6261e86e0c2c343bd564ce403f
Joe Beaumont <joseph.beaumont@live.co.uk>
2025-08-07 15:36:48 +0100

    Fixed issue with api

--- src/Api.jsx
+++ src/Api.jsx
@@ -24,7 +24,7 @@ export function getArticles(article_id) {
                 return response.data.article
             })
             .catch((error) => {
-                throw new Error("Error: We could not find that article")
+                throw new Error ("Error: We could not find that article")
             })
     } else {
         return axios.get("https://joe-beaumont-nc-news.onrender.com/api/articles")
@@ -32,7 +32,7 @@ export function getArticles(article_id) {
                 return response.data.articles
             })
             .catch((error) => {
-                throw new Error("Error: We could not find any articles")
+                throw new Error ("Error: We could not find any articles")
             })
     }
 }
@@ -43,71 +43,60 @@ export function getCommentsById(article_id) {
             return response.data.comments
         })
         .catch((error) => {
-            throw new Error("Error: We could not find any comments")
+            throw new Error ("Error: We could not find any comments")
         })
 }
 
 
 export function incrementVotesOnArticle(inc_votes, article_id) {
     return axios.patch(`https://joe-beaumont-nc-news.onrender.com/api/articles/${article_id}`, inc_votes)
-        .catch((error) => {
-            throw new Error("Error: Vote unsuccessful")
-        })
+    .catch((error) => {
+        throw new Error ("Error: Vote unsuccessful")
+    })
 }
 
-export function postNewComment(request) {
+export function postNewComment(request){
     const article_id = request.params
     return axios.post(`https://joe-beaumont-nc-news.onrender.com/api/articles/${article_id}/comments`, request.body)
-        .then((response) => {
-            console.log(response)
-        })
-        .catch((error) => {
-            console.log(error)
-            throw new Error("Error: Comment could not be posted")
-        })
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((error) => {
+        console.log(error)
+        throw new Error ("Error: Comment could not be posted")
+    })
 }
 
-export function deleteCommentAPI(comment_id) {
+export function deleteCommentAPI(comment_id){
     return axios.delete(`https://joe-beaumont-nc-news.onrender.com/api/comments/${comment_id}`)
-        .then((response) => {
-            console.log(response)
-        })
-        .catch((error) => {
-            console.log(error)
-            throw new Error("Error: Comment could not be deleted")
-        })
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((error) => {
+        console.log(error)
+        throw new Error ("Error: Comment could not be deleted")
+    })
 }
 
-export function getTopics() {
+export function getTopics(){
     return axios.get(`https://joe-beaumont-nc-news.onrender.com/api/topics`)
-        .then((response) => {
-            return response.data.topics
-        })
-        .catch((error) => {
-            console.log(error)
-            throw new Error("Error: No topics found")
-        })
+    .then((response) => {
+        return response.data.topics
+    })
+    .catch((error) => {
+        console.log(error)
+        throw new Error ("Error: No topics found")
+    })
 }
 
-export function ArticleQueries({ filter, by, sort, order }) {
-    let url = "https://joe-beaumont-nc-news.onrender.com/api/articles"
-    if (filter) {
-        url += `?filter=${filter}&by=${by}`
-    }
-    if (sort) {
-        if (filter) {
-            url += `&sort=${sort}&order=${order}`
-        } else {
-            url += `?sort=${sort}&order=${order}`
-        }
-    }
-
-    return axios.get(url)
-        .then((response) => {
-            return response.data.articles
-        })
-        .catch((error) => {
-            console.log(error)
-            throw new Error("Error: No articles found")
-        })
+export function getArticlesQueries(filter, by){
+ 
+    return axios.get(`https://joe-beaumont-nc-news.onrender.com/api/articles?filter=${filter}&by=${by}`)
+    .then((response) => {
+        return response.data.articles
+    })
+    .catch((error) => {
+        console.log(error)
+        throw new Error ("Error: No topics found")
+    })
 }
